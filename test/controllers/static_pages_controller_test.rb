09a48f418866815cc3a558000b8882a6eb23f5f2
Collin <collinc777@aol.com>
2014-12-29 14:45:25 -0600

    set homepage without any styling or content

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,4 +6,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  #to add a page first lets test its creation
+=begin
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+=end
+
 end
